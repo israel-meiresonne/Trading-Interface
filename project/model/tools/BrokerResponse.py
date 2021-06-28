04ed7423dc3495e2100aade5763ae8262a5df279
Israel Meiresonne <israelmeiresonne97@gmail.com>
2021-06-28 23:05:28 +0300

    BrokerResponse: make BrokerResponse inherits of MyJson

--- project/model/tools/BrokerResponse.py
+++ project/model/tools/BrokerResponse.py
@@ -1,27 +1,20 @@
 from json import loads as json_loads
 from requests import Response
 
+from model.tools.MyJson import MyJson
 from model.tools.RequestResponse import RequestResponse
 
 
-class BrokerResponse(RequestResponse):
+class BrokerResponse(RequestResponse, MyJson):
     def __init__(self, rsp: Response):
         RequestResponse.__init__(self, rsp)
 
     def get_content(self) -> [list, dict]:
         return json_loads(super(BrokerResponse, self).get_content())
 
-    def __str__(self):
-        rsp = self._get_response()
-        rsp_dict = rsp.__dict__.items()
-        opt = "\n—————\n|Response|\n—————\n"
-        for k, v in rsp_dict:
-            opt += f"{k}: {v}\n"
-            if k == "request":
-                v_dict = v.__dict__
-                opt += "*****\n|Response.request|\n*****\n"
-                for v_k, v_v in v_dict.items():
-                    opt += f"{v_k}: {v_v}\n"
-                opt += "*****\n|END Response.request|\n*****\n"
-        opt += "—————\n|END Response|\n—————"
-        return opt
+    @staticmethod
+    def json_instantiate(object_dic: dict) -> object:
+        _class_token = MyJson.get_class_name_token()
+        instance = BrokerResponse(Response())
+        exec(MyJson.get_executable())
+        return instance
