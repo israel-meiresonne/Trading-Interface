3cb0e1b6a16d202635657de85c4cf91d4d470447
Israel Meiresonne <israelmeiresonne97@gmail.com>
2021-07-06 10:57:03 +0200

    add bash executables to configure new environement

---
+++ project/content/executable/apollo21/content_mkdir.sh
@@ -0,0 +1,4 @@
+cd apollo21/project/../.. && mkdir -p apollo21/project/content/v0.01/tests/
+cd apollo21/project/../.. && mkdir -p apollo21/project/content/storage/STAGE_1/Bot/
+cd apollo21/project/../.. && mkdir -p apollo21/project/content/storage/STAGE_2/Bot/
+cd apollo21/project/../.. && mkdir -p apollo21/project/content/storage/STAGE_3/Bot/
