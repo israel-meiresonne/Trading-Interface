1c824170bc82427a1804e3a6a9600df9d3922522
Israel Meiresonne <israelmeiresonne97@gmail.com>
2021-09-05 16:09:17 +0300

    Strategy: make class abstract

--- project/model/structure/Strategy.py
+++ project/model/structure/Strategy.py
@@ -1,4 +1,4 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 from typing import List
 
 from config.Config import Config
@@ -14,7 +14,7 @@ from model.tools.Pair import Pair
 from model.tools.Price import Price
 
 
-class Strategy:
+class Strategy(ABC):
     _CONST_BOT_SLEEP_TIME = 60
     _PERFORMANCE_INIT_CAPITAL = 100
     _TOP_ASSET = None
