f396cd07429646a2069d14b96d832532a068236b
Israel Meiresonne <israelmeiresonne97@gmail.com>
2021-09-22 19:47:24 +0300

    MachineLearning: add ML class and its tests

---
+++ project/model/tools/MachineLearning.py
@@ -0,0 +1,269 @@
+from typing import List, Union
+
+import numpy as np
+from model.structure.database.ModelFeature import ModelFeature as _MF
+from model.tools.MyJson import MyJson
+
+
+class ML(MyJson):
+    _DEBUG = True
+    _COEF_ALPHA_UP = 10
+    _COEF_ALPHA_DOWN = _COEF_ALPHA_UP
+
+    def __init__(self, ys: List[list], xs: List[list], degree: int = None) -> None:
+        self.__ys = None
+        self.__xs = None
+        self.__degree = None
+        self.__X = None
+        self.__theta = None
+        self.__trained = False
+        self.__cost_historic = None
+        self.__cost_rates = None
+        self._set_dataset(ys, xs)
+        self._set_degree(degree)
+
+    def _set_dataset(self, ys: List[list], xs: List[list]) -> None:
+        ys = np.array(ys)
+        if (ys.shape != (ys.shape[0],)) and (ys.shape != (ys.shape[0],1)):
+            raise ValueError(f"Dataset's Ys must have shape '{(ys.shape[0],)}' or '{(ys.shape[0],1)}', instead '{ys.shape}'")
+        ys = ys.reshape(ys.shape[0], 1)
+        xs = np.array(xs)
+        xs = ys.reshape(xs.shape[0], 1) if xs.shape == (xs.shape[0],) else xs
+        if xs.shape != (ys.shape[0],xs.shape[1]):
+            raise ValueError(f"Dataset's Xs must have shape '{(ys.shape[0],xs.shape[1])}', instead '{xs.shape}'")
+        self.__ys = ys
+        self.__xs = xs
+    
+    def get_ys(self) -> np.ndarray:
+        return self.__ys
+    
+    def get_xs(self) -> np.ndarray:
+        return self.__xs
+
+    def _set_degree(self, degree: int = None) -> None:
+        self._check_degree(degree) if degree is not None else None
+        self.__degree = degree
+    
+    @staticmethod
+    def _check_degree(degree: int) -> None:
+        if (not isinstance(degree, int)) or (degree <= 0):
+            raise ValueError(f"Degree must be type int and > 0, instead '{degree}'({type(degree)})")
+    
+    def _search_degree(self) -> None:
+        raise Exception(f"Must implement")
+    
+    def get_degree(self) -> int:
+        self._search_degree() if self.__degree is None else None
+        return self.__degree
+    
+    def _set_X(self) -> None:
+        degree = self.get_degree()
+        xs = self.get_xs()
+        self.__X = self.generate_X(xs, degree)
+    
+    def get_X(self) -> np.ndarray:
+        """
+        To get matrix X\n
+        return: X
+            Matrix X
+        """
+        self._set_X() if self.__X is None else None
+        return self.__X
+    
+    def _set_theta(self, theta: np.ndarray = None) -> None:
+        if theta is None:
+            n_param = self.get_X().shape[1]
+            self.__theta = np.zeros((n_param, 1))
+        else:
+            self.__theta = theta
+    
+    def get_theta(self) -> np.ndarray:
+        self._set_theta() if self.__theta is None else None
+        return self.__theta
+    
+    def _set_trained(self, is_trained: bool) -> None:
+        self.__trained = is_trained
+    
+    def is_trained(self) -> bool:
+        return self.__trained
+    
+    def _set_cost_historic(self, cost_historic: np.ndarray) -> None:
+        self.__cost_historic = cost_historic
+    
+    def get_cost_historic(self) -> np.ndarray:
+        return self.__cost_historic
+    
+    def _set_cost_rates(self, cost_rates: np.ndarray) -> None:
+        self.__cost_rates = cost_rates
+    
+    def get_cost_rates(self) -> np.ndarray:
+        return self.__cost_rates
+    
+    def _set_coef_determination(self) -> None:
+        self.__coef_determination = self.coef_determination(self.get_ys(), self.predict(self.get_xs()))
+
+    def get_coef_determination(self) -> float:
+        return self.__coef_determination
+    
+    def train(self) -> None:
+        X = self.get_X()
+        ys = self.get_ys()
+        theta = self.get_theta()
+        new_theta, cost_hist, cost_rates = self.gradient_decsent(X, ys, theta)
+        self._set_theta(new_theta)
+        self._set_cost_historic(cost_hist)
+        self._set_cost_rates(cost_rates)
+        self._set_trained(is_trained=True)
+        self._set_coef_determination()
+    
+    def predict(self, xs: List[list]) -> np.ndarray:
+        """
+        To predit Y values of given sample\n
+        xs: List[list]
+            Samples
+        return: np.ndarray
+            Y values prdicted
+        """
+        xs = np.array(xs)
+        if xs.shape[1] != self.get_xs().shape[1]:
+            raise ValueError(f"Dataset's Xs must have '{self.get_xs().shape[1]}' column, instead '{xs.shape[1]}'")
+        self.train() if not self.is_trained() else None
+        X = self.generate_X(xs, self.get_degree())
+        return ML.model(X, self.get_theta())
+    
+    @staticmethod
+    def generate_X(xs: Union[List[list], np.ndarray], degree: int) -> np.ndarray:
+        ML._check_degree(degree)
+        xs = np.array(xs) if not isinstance(xs, np.ndarray) else xs
+        tab = [xs**i for i in range(degree, 0, -1)]
+        tup = tuple([*tab, np.ones((xs.shape[0], 1))])
+        return np.hstack(tup)
+    
+    @staticmethod
+    def model(X: np.ndarray, theta: np.ndarray) -> np.ndarray:
+        """
+        To generate model like F = X.Theta\n
+        X: np.ndarray
+            Matrix X
+        theta: np.ndarray
+            Matrix theta
+        return: model
+            Model like F = X.Theta
+        """
+        return X.dot(theta)
+    
+    @staticmethod
+    def cost_function(X, ys, theta) -> float:
+        m = len(ys)
+        F = ML.model(X, theta)
+        return 1/(2*m) * np.sum((F - ys)**2)
+    
+    @staticmethod
+    def gradient(X, y, theta) -> float:
+        m = len(y)
+        F = ML.model(X, theta)
+        return 1/m * X.T.dot(F - y)
+
+    @staticmethod
+    def gradient_decsent(X: np.ndarray, ys: np.ndarray, theta: np.ndarray, alpha: float = None, n_lesson: int = None) -> tuple:
+        def get_new_theta(func_alpha: float, func_theta: np.ndarray) -> np.ndarray:
+                func_new_theta = func_theta - func_alpha * ML.gradient(X, ys, func_theta)
+                return func_new_theta
+
+        def get_rate(new: float, hold: float) -> float:
+            return new / hold - 1
+
+        def get_coef(func_theta: np.ndarray) -> float:
+            predictions = ML.model(X, func_theta)
+            return ML.coef_determination(ys, predictions)
+        
+        def reduce_alpha(func_alpha: float) -> float:
+            return func_alpha / ML._COEF_ALPHA_DOWN
+
+        def increase_alpha(func_alpha: float) -> float:
+            return func_alpha * ML._COEF_ALPHA_UP
+
+        def init_alpha(func_theta: np.ndarray) -> float:
+            func_alpha = 1
+            last_cost = ML.cost_function(X, ys, func_theta)
+            func_new_theta = get_new_theta(func_alpha, func_theta)
+            new_cost = ML.cost_function(X, ys, func_new_theta)
+            cost_dropping = new_cost <= last_cost
+            if cost_dropping:
+                # search rising cost
+                cost_rising = False
+                while not cost_rising:
+                    last_cost = ML.cost_function(X, ys, func_theta)
+                    func_new_theta = get_new_theta(func_alpha, func_theta)
+                    new_cost = ML.cost_function(X, ys, func_new_theta)
+                    cost_rising = new_cost > last_cost
+                    func_alpha = increase_alpha(func_alpha) if not cost_rising else reduce_alpha(func_alpha)
+            else:
+                # search dropping cost
+                cost_dropping = False
+                while not cost_dropping:
+                    last_cost = ML.cost_function(X, ys, func_theta)
+                    func_new_theta = get_new_theta(func_alpha, func_theta)
+                    new_cost = ML.cost_function(X, ys, func_new_theta)
+                    cost_dropping = new_cost <= last_cost
+                    func_alpha = reduce_alpha(func_alpha) if not cost_dropping else func_alpha
+            print(f"final alpha: {func_alpha}") if ML._DEBUG else None
+            return func_alpha
+
+        cost_hist = []
+        cost_rates = []
+        perf_coefs = []
+        new_theta = np.array(theta.tolist())
+        if n_lesson is not None:
+            for i in range(n_lesson):
+                new_theta = new_theta - alpha * ML.gradient(X, ys, new_theta)
+                cost_hist.append(ML.cost_function(X, ys, new_theta))
+                cost_rates.append(cost_hist[i]/cost_hist[i-1]-1) if (i > 0) else None
+        else:
+            # Found max alpha
+            alpha = init_alpha(new_theta)
+            # Train
+            cost_rate_limit = 10**(-4)
+            perf_coef_limit = 0.005
+            perf_coef_min = 0.5
+            perf_coefs.append(get_coef(new_theta))
+            cost = ML.cost_function(X, ys, new_theta)
+            cost_hist.append(cost)
+            end = False
+            i = 0
+            while not end:
+                # Update
+                new_theta = get_new_theta(alpha, new_theta)
+                cost = ML.cost_function(X, ys, new_theta)
+                cost_hist.append(cost)
+                cost_rates.append(get_rate(cost, cost_hist[-2])) if len(cost_hist) >= 2 else cost_rates.append(cost_rate_limit * 2)
+                # Check limit
+                cost_rising = cost >= cost_hist[-2]
+                cost_rate_bellow_limit = abs(cost_rates[-1]) <= cost_rate_limit
+                if cost_rising:
+                    alpha = reduce_alpha(alpha)
+                elif cost_rate_bellow_limit:
+                    perf_coefs.append(get_coef(new_theta))
+                    delta_coef = abs(perf_coefs[-1] - perf_coefs[-2])
+                    end = (perf_coefs[-1] >= perf_coef_min) and (delta_coef <= perf_coef_limit)
+                    if not end:
+                        alpha = reduce_alpha(alpha)
+                end = True if (not end) and (i >= 5*10**(4)) else end
+                print(f"i: {i}") if ML._DEBUG else None
+                print(f"alpha: {alpha}") if ML._DEBUG else None
+                print(f"cost: {cost}") if ML._DEBUG else None
+                i += 1
+        return new_theta, cost_hist, cost_rates
+    
+    @staticmethod
+    def coef_determination(ys: np.ndarray, predictions: np.ndarray) -> float:
+        u = ((ys - predictions)**2).sum()
+        v = ((ys - ys.mean())**2).sum()
+        return 1 - u/v
+    
+    @staticmethod
+    def json_instantiate(object_dic: dict) -> object:
+        _class_token = MyJson.get_class_name_token()
+        instance = ML([],[])
+        exec(MyJson.get_executable())
+        return instance
---
+++ project/tests/tools/TestMachineLearning.py
@@ -0,0 +1,154 @@
+import unittest
+
+import matplotlib.pyplot as plt
+import numpy as np
+from model.structure.database.ModelFeature import ModelFeature as _MF
+from model.tools.FileManager import FileManager
+from model.tools.MachineLearning import ML
+
+
+class TestMachineLearning(unittest.TestCase, ML):
+    def setUp(self) -> None:
+        # Degree 1
+        self.coef1 = 2
+        self.y1 = 1
+        a = self.coef1
+        b = self.y1
+        self.xs1 = [[x] for x in range(1, 51)]
+        self.ys1 = [[a * x[0] + b] for x in self.xs1]
+        self.ml1 = ML(self.ys1, self.xs1, degree=1)
+        # Degree 2
+        a = 3
+        b = 5
+        self.xs2 = [[x] for x in range(1, 51)]
+        self.ys2 = [[a * x[0]**2 + b] for x in self.xs1]
+        self.ml2 = ML(self.ys2, self.xs2, degree=2)
+        # Degree 11
+        degree = 11
+        self.xs11 = [[x] for x in range(1, 51)]
+        self.ys11 = [[a * x[0]**degree + b] for x in self.xs11]
+        self.ml11 = ML(self.ys11, self.xs11, degree=degree)
+
+    def test_set_dataset(self) -> None:
+        ml = ML(self.ys1, self.xs1, degree=1)
+        # Check Ys
+        exp1 = np.array(self.ys1).reshape((len(self.ys1), 1))
+        result1 = ml.get_ys()
+        self.assertListEqual(exp1.tolist(), result1.tolist())
+        self.assertEqual(exp1.shape, result1.shape)
+        # Check Xs
+        exp2 = np.array(self.xs1).reshape((len(self.xs1), 1))
+        result2 = ml.get_xs()
+        self.assertListEqual(exp2.tolist(), result2.tolist())
+        self.assertEqual(exp2.shape, result2.shape)
+        # Wrong Ys shape
+        xs = [[x] for x in range(50)]
+        ys = [[x[0]*2, x] for x in xs]
+        with self.assertRaises(ValueError):
+            ML(ys, xs, 1)
+        # Wrong Xs shape
+        xs = [[x] for x in range(50)]
+        ys = [[x[0]*2] for x in xs]
+        del xs[-5:-1]
+        with self.assertRaises(ValueError):
+            ML(ys, xs, 1)
+
+    def test_set_degree(self) -> None:
+        ml = ML(self.ys1, self.xs1, degree=1)
+        self.assertEqual(1, ml.get_degree())
+        # Degree is None
+        with self.assertRaises(Exception):
+            ml = ML(self.ys1, self.xs1, degree=None)
+            ml.get_degree()
+        # Degree < 0
+        with self.assertRaises(ValueError):
+            ml = ML(self.ys1, self.xs1, degree=-1)
+        # Degree == 0
+        with self.assertRaises(ValueError):
+            ml = ML(self.ys1, self.xs1, degree=0)
+
+    def test_get_X(self) -> None:
+        ml = self.ml1
+        exp = len(self.xs1[0]) * ml.get_degree() + 1
+        self.assertEqual(exp, ml.get_X().shape[1])
+
+    def test_set_theta(self) -> None:
+        ml1 = self.ml1
+        # Tehta is None
+        ml1._set_theta(theta=None)
+        theta1 = ml1.get_theta()
+        self.assertIsInstance(theta1, np.ndarray)
+        # Theta is not None
+        ml2 = ML(self.ys1, self.xs1, degree=1)
+        n_param = ml2.get_X().shape[1]
+        exp2 = np.random.randn(n_param, 1)
+        ml2._set_theta(theta=exp2)
+        result2 = ml2.get_theta()
+        self.assertListEqual(exp2.tolist(), result2.tolist())
+        self.assertEqual(id(exp2), id(result2))
+
+    def test_predict(self) -> None:
+        self.setUp()
+        print_dir = FileManager.get_project_directory() + "tests/"
+        a = self.coef1
+        b = self.y1
+        if True:
+            ml1 = ML(self.ys1, self.xs1, degree=1)
+            perf_coef = ml1.get_coef_determination()
+            self.assertTrue(perf_coef >= 0.9)
+            print(perf_coef)
+        # Graph
+        if False:
+            plt.scatter(self.xs1, self.ys1)
+            predictions = ml1.predict(self.xs1)
+            plt.plot(self.xs1, predictions, 'r')
+            plt.savefig('../graph1.png')
+        # Graph 2
+        if False:
+            ml2 = self.ml2
+            plt.figure()
+            plt.scatter(ml2.get_xs(), ml2.get_ys())
+            predictions = ml2.predict(ml2.get_xs())
+            plt.plot(ml2.get_xs(), predictions, 'r')
+            plt.savefig('../graph2.png')
+        # Graph X
+        if False:
+            degree = 1
+            xs = [[x] for x in range(-25, 25)]
+            ys = [[a * x[0] ** degree + b] for x in xs]
+            ml = ML(ys, xs, degree=degree)
+            plt.figure()
+            predictions = ml.predict(ml.get_xs())
+            plt.scatter(ml.get_xs(), ml.get_ys())
+            plt.plot(ml.get_xs(), predictions, 'r')
+            plt.savefig(f'{print_dir}graphX-{degree}.png')
+            print(f"coef_determ: {ml.get_coef_determination()}")
+        # Graph 11
+        if False:
+            ml11 = self.ml11
+            plt.figure()
+            plt.scatter(ml11.get_xs(), ml11.get_ys())
+            predictions = ml11.predict(ml11.get_xs())
+            plt.plot(ml11.get_xs(), predictions, 'r')
+            plt.savefig('../graph3.png')
+
+    def test_generate_X(self) -> None:
+        # n_features=1 AND Degree=1
+        xs1 = [[2], [5]]
+        exp1 = _MF.json_decode(_MF.json_encode(xs1))
+        [row.append(1) for row in exp1]
+        result1 = ML.generate_X(xs1, degree=1)
+        self.assertListEqual(exp1, result1.tolist())
+        # n_features=2 AND Degree=1
+        xs2 = [[2, 3], [5, 7]]
+        exp2 = _MF.json_decode(_MF.json_encode(xs2))
+        [row.append(1) for row in exp2]
+        result2 = ML.generate_X(xs2, degree=1)
+        self.assertListEqual(exp2, result2.tolist())
+        # n_features=2 AND Degree=3
+        xs3 = [[2, 3], [5, 7]]
+        np_xs3 = np.array(xs3)
+        exp3 = np.hstack((np_xs3**3, np_xs3**2, np_xs3,
+                         np.ones((np_xs3.shape[0], 1))))
+        result3 = ML.generate_X(xs3, degree=3)
+        self.assertListEqual(exp3.tolist(), result3.tolist())
