91a270b8c5147b847e927416c73a2c9b053c3d04
Israel Meiresonne <israelmeiresonne97@gmail.com>
2021-10-09 19:46:22 +0300

    Pair: add function to format Pair into a string in the given format

--- project/tests/tools/TestPair.py
+++ project/tests/tools/TestPair.py
@@ -12,19 +12,25 @@ class TestPair(unittest.TestCase, Pair):
         self.exp_rasset = Asset(self.rsbl)
         self.pr = Pair(self.lsbl, self.rsbl)
 
-    def test_constructor_with_one_param(self):
+    def test_constructor_with_one_param(self) -> None:
         pr = Pair(self.prsbl)
         self.assertDictEqual(self.exp_lasset.__dict__, pr.get_left().__dict__)
         self.assertDictEqual(self.exp_rasset.__dict__, pr.get_right().__dict__)
 
-    def test_constructor_with_two_params(self):
+    def test_constructor_with_two_params(self) -> None:
         pr = Pair(self.lsbl, self.rsbl)
         self.assertDictEqual(self.exp_lasset.__dict__, pr.get_left().__dict__)
         self.assertDictEqual(self.exp_rasset.__dict__, pr.get_right().__dict__)
 
-    def test_get_merges_symbols(self):
+    def test_get_merges_symbols(self) -> None:
         exp = (self.lsbl + self.rsbl).lower()
         self.assertEqual(exp, self.pr.get_merged_symbols())
+    
+    def test_format(self) -> None:
+        pair = self.pr
+        exp1 = f"{pair.get_left()}%{pair.get_right()}"
+        result1 = pair.format(f"{Pair.LEFT}%{Pair.RIGHT}")
+        self.assertEqual(exp1, result1)
 
     def test_json_encode_decode(self) -> None:
         original_obj = self.pr
