b0800f0360efcbd135a6f7f0c8a5b2fcde8772c8
Israel Meiresonne <israelmeiresonne97@gmail.com>
2021-10-09 20:47:51 +0300

    Predictor: add class

--- project/test.py
+++ project/test.py
@@ -1,5 +1,18 @@
-from tests.tools.TestMachineLearning import TestMachineLearning
 
-test = TestMachineLearning()
-test.setUp()
-test.test_search_degree()
+def dynamic_exec(class_name: str, test_func: str) -> None:
+    exec(f'from tests.tools.{class_name} import {class_name}')
+    test_obj = eval(f'{class_name}()')
+    test_obj.setUp()
+    exec(f'test_obj.{test_func}()')
+    print('\033[32m')
+    print(f"Test '{test_func}' of class '{class_name}' exceted!")
+    print('\033[0m')
+
+
+# dynamic_exec(class_name='TestDeepLearning', test_func='test_save')
+# dynamic_exec(class_name='TestDeepLearning', test_func='test_predict')
+# dynamic_exec(class_name='TestDeepLearning', test_func='test_perso')
+
+dynamic_exec(class_name='TestPredictor', test_func='test_learn')
+# dynamic_exec(class_name='TestPredictor', test_func='test_predict')
+# dynamic_exec(class_name='TestPredictor', test_func='test_add_learns')
