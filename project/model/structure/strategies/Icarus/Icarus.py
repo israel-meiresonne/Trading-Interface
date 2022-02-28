264e067fc634376529d9ad1f11273015301d22bd
Israel Meiresonne <israelmeiresonne97@gmail.com>
2022-02-28 21:21:49 +0200

    Icarus: use MACD's tangent to sell position

--- project/model/structure/strategies/Icarus/Icarus.py
+++ project/model/structure/strategies/Icarus/Icarus.py
@@ -416,7 +416,7 @@ class Icarus(TraderClass):
             histogram.reverse()
             histogram_dropping = histogram[-1] < 0
             return histogram_dropping
-        """
+
         def are_macd_signal_negatives(vars_map: Map) -> bool:
             macd_map = marketprice.get_macd()
             macd = list(macd_map.get(Map.macd))
@@ -432,13 +432,11 @@ class Icarus(TraderClass):
             macd.reverse()
             tangent_macd_dropping = macd[-1] <= macd[-2]
             return tangent_macd_dropping
-        """
 
         vars_map = Map()
         can_sell = False
         # Check
-        # can_sell = (not is_buy_period()) and (is_histogram_dropping(vars_map) or (are_macd_signal_negatives(vars_map) and is_tangent_macd_dropping(vars_map)))
-        can_sell = (not is_buy_period()) and is_histogram_dropping(vars_map)
+        can_sell = (not is_buy_period()) and (is_histogram_dropping(vars_map) or (are_macd_signal_negatives(vars_map) and is_tangent_macd_dropping(vars_map)))
         return can_sell
 
     def _can_sell_prediction(self, predictor_marketprice: MarketPrice, marketprice: MarketPrice) -> bool:
@@ -972,7 +970,7 @@ class Icarus(TraderClass):
                 histogram.reverse()
                 histogram_dropping = histogram[-1] < 0
                 return histogram_dropping
-            """
+
             def are_macd_signal_negatives(vars_map: Map) -> bool:
                 macd_map = marketprice.get_macd()
                 macd = list(macd_map.get(Map.macd))
@@ -988,12 +986,11 @@ class Icarus(TraderClass):
                 macd.reverse()
                 tangent_macd_dropping = macd[-1] <= macd[-2]
                 return tangent_macd_dropping
-            """
 
             vars_map = Map()
             can_sell = False
             # Check
-            can_sell = (not is_buy_period()) and is_histogram_dropping(vars_map)
+            can_sell = (not is_buy_period()) and (is_histogram_dropping(vars_map) or (are_macd_signal_negatives(vars_map) and is_tangent_macd_dropping(vars_map)))
             return can_sell
 
         def trade_history(pair: Pair, period: int)  -> pd.DataFrame:
