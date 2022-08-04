776f795a956333b3f72981a5b6fbfb0983be081a
Israel Meiresonne <israelmeiresonne97@gmail.com>
2022-08-04 09:34:34 +0300

    MyJson: don't compare object's attribut 'id' in function .__eq__()

--- project/tests/tools/TestMyJson.py
+++ project/tests/tools/TestMyJson.py
@@ -1,4 +1,5 @@
 import unittest
+from model.tools.Map import Map
 
 from model.tools.MyJson import MyJson
 from model.structure.database.ModelFeature import ModelFeature as _MF
@@ -8,6 +9,7 @@ from model.tools.Price import Price
 class TestMyJson(unittest.TestCase):
     def setUp(self) -> None:
         self.price = Price(19, 'USDT')
+        self.map1 = Map({'k1': 'v1'})
 
     def test_get_import(self) -> None:
         # Check import of class file
@@ -15,8 +17,8 @@ class TestMyJson(unittest.TestCase):
         result1 = MyJson.get_import('MinMax')
         self.assertEqual(exp1, result1)
         # Check import of class folder
-        exp2 = 'from model.structure.strategies.Stalker.Stalker import Stalker'
-        result2 = MyJson.get_import('Stalker')
+        exp2 = 'from model.structure.strategies.Icarus.Icarus import Icarus'
+        result2 = MyJson.get_import('Icarus')
         self.assertEqual(exp2, result2)
         # Execute all import
         imports = MyJson.get_imports()
@@ -42,6 +44,17 @@ class TestMyJson(unittest.TestCase):
         with self.assertRaises(ValueError):
             MyJson.json_decode(_MF.json_encode([]))
 
+    def test_copy(self) -> None:
+        def test(obj: MyJson) -> None:
+            obj_dict = obj.__dict__.copy()
+            obj_copy = obj.copy()
+            self.assertNotEqual(id(obj), id(obj_copy))
+            self.assertEqual(obj, obj_copy)
+            self.assertDictEqual(obj_dict, obj_copy.__dict__)
+
+        test(self.price)
+        test(self.map1)
+
     def test_generate_instance(self) -> None:
         prc = self.price
         json_str = prc.json_encode()
@@ -50,6 +63,29 @@ class TestMyJson(unittest.TestCase):
         new_json_str = _MF.json_encode(json_dic)
         with self.assertRaises(KeyError):
             MyJson.json_decode(new_json_str)
+
+    def test__eq__(self) -> None:
+        def test(obj1: object, obj2: object, obj3: object) -> None:
+            obj1_copy = obj1.__dict__.copy()
+            obj2_copy = obj2.__dict__.copy()
+            obj3_copy = obj3.__dict__.copy()
+            # Object with an attribut 'id'
+            self.assertEqual(obj1, obj2)
+            self.assertNotEqual(obj1, obj3)
+            self.assertDictEqual(obj1.__dict__, obj1_copy)
+            self.assertDictEqual(obj2.__dict__, obj2_copy)
+            self.assertDictEqual(obj3.__dict__, obj3_copy)
+
+        # Object with an attribut 'id'
+        map1 = Map({'k1': 'v1'})
+        map2 = Map({'k1': 'v1'})
+        map3 = Map({'k3': 'v3'})
+        test(map1, map2, map3)
+        # Object without an attribut 'id'
+        price1 = Price(1, 'USDT')
+        price2 = Price(1, 'USDT')
+        price3 = Price(3, 'USDT')
+        test(price1, price2, price3)
 
 
 if __name__ == '__main__':
