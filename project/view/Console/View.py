4547fc7a18098e4504458994cb2e4e6d382e753a
Israel Meiresonne <israelmeiresonne97@gmail.com>
2022-10-13 19:02:22 +0300

    View: add color on ouputs

--- project/view/Console/View.py
+++ project/view/Console/View.py
@@ -3,6 +3,13 @@ from typing import Any
 
 
 class View:
+    SEPARATOR = "——————————————————————————————"
+    C_NORMAL =  '\033[0m'
+    C_YELLOW =  '\033[33m'
+    C_PURPLE =  '\033[35m'
+    C_RED =     '\033[31m'
+    C_GREEN =   '\033[32m'
+
     def input(self, message: str = None, type_func: Any = None, secure: bool = False) -> Any:
         """
         To input value
@@ -22,10 +29,11 @@ class View:
             The input typed
         """
         message = message + "\n" if message is not None else ""
+        input_sufix = f'{self.C_PURPLE}>>>{self.C_NORMAL} '
         if secure:
-            entry = getpass.getpass(message)
+            entry = getpass.getpass(message + input_sufix)
         else:
-            entry = input(message)
+            entry = input(message + input_sufix)
         if (not secure) and (type_func is not None):
             converted = False
             while not converted:
@@ -34,7 +42,7 @@ class View:
                     converted = True
                 except Exception as e:
                     self.output(f"Can't convert entry '{entry}' into type '{type_func}'", is_error=True)
-                    entry = input()
+                    entry = input(input_sufix)
         return entry
 
     def menu(self, message: str, options: list) -> int:
@@ -58,6 +66,9 @@ class View:
                 o = f"{i}. {choices[i]}"
                 self.output(o)
 
+        if len(options) == 0:
+            raise ValueError(f"The list of option can't be empty")
+        self.output(self.SEPARATOR)
         self.output(message)
         display_choices(options)
         n_choice = len(options)
@@ -69,11 +80,12 @@ class View:
                 success = (0 <= entry < n_choice)
             else:
                 success = entry in options
-            self.output(f"Option '{entry}' don't exit in menu", is_error=True) if not success else None
+            self.output(f"This option '{entry}' don't exist in menu", is_error=True) if not success else None
         index = entry if isinstance(entry, int) else options.index(entry)
+        self.output(self.SEPARATOR)
         return index
 
-    def output(self, out: Any, is_error: bool = False) -> None:
+    def output(self, out: Any, is_error: bool = False, is_success: bool = False) -> None:
         """
         To display datas
 
@@ -84,5 +96,11 @@ class View:
         is_error: bool = False
             Set True to display error message else False
         """
-        print('\033[93m', out, '\033[0m') if is_error else print(out)
+        if is_success:
+            print(self.C_GREEN + out + self.C_NORMAL)
+            return None
+        if is_error:
+            print(self.C_RED + out + self.C_NORMAL)
+            return None
+        print(out)
 
