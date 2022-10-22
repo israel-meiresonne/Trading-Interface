7b0591db798605ab645590ac5adc8472889b5ace
Israel Meiresonne <israelmeiresonne97@gmail.com>
2022-10-22 18:39:06 +0300

    test: improve dynamic import of test files

--- project/tests/test.py
+++ project/tests/test.py
@@ -4,28 +4,7 @@ def push_path() -> None:
     project_dir = '/Users/israelmeiresonne/MY-MAC/ROQUETS/companies/apollo21/dev/apollo21/dev/apollo21/project/'
     sys.path.append(project_dir) if project_dir not in sys.path else None
 
-def get_import(class_name: str) -> str:
-    imports = [
-        f'from tests.API.brokers.{class_name}.{class_name} import {class_name}',
-        f'from tests.structure.database.{class_name} import {class_name}',
-        f'from tests.structure.strategies.{class_name} import {class_name}',
-        f'from tests.structure.strategies.' + class_name.replace('Test', '') + f'.{class_name} import {class_name}',
-        f'from tests.tools.{class_name} import {class_name}'
-    ]
-    found = None
-    i = 0
-    while not found:
-        try:
-            exec(imports[i])
-            break
-        except ModuleNotFoundError as e:
-            i += 1
-            if i >= len(imports):
-                raise e
-    return imports[i]
-
 def extract_tests(class_ref: Any) -> List[str]:
-    from model.structure.database.ModelFeature import ModelFeature as _MF
     regex = r'^test_.*$'
     str_funcs = dir(class_ref)
     tests = [str_func for str_func in str_funcs if _MF.regex_match(regex, str_func)]
@@ -43,7 +22,8 @@ def dynamic_exec(class_ref: Any, str_func: str) -> None:
     print('\033[0m')
 
 def run_Test(class_name: str, str_func: str = None) -> None:
-    exec(get_import(class_name))
+    _MF._IMPORT_ROOTS.append('tests/')
+    exec(_MF.get_import(class_name))
     class_ref = eval(class_name)
     str_funcs = extract_tests(class_ref)
     if str_func is None:
@@ -54,4 +34,5 @@ def run_Test(class_name: str, str_func: str = None) -> None:
 
 if __name__ == '__main__':
     push_path()
+    from model.structure.database.ModelFeature import ModelFeature as _MF
     run_Test(class_name='', str_func='')
