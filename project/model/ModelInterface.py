d4502cae49832b38c66081e146c5f7391d87ae64
Israel Meiresonne <israelmeiresonne97@gmail.com>
2022-10-23 10:44:11 +0300

    ModelInterface: update name of function and their docs

--- project/model/ModelInterface.py
+++ project/model/ModelInterface.py
@@ -1,34 +1,59 @@
 from abc import ABC, abstractmethod
 
-from model.structure.Bot import Bot
 from model.structure.database.ModelFeature import ModelFeature as _MF
 
 
 class ModelInterface(ABC):
-    def __init__(self, prefix: str):
-        self.log_id = prefix + _MF.new_code()
-        self.__settime = _MF.get_timestamp(_MF.TIME_MILLISEC)
+    def __init__(self):
+        self.__id =         None
+        self.__settime =    None
+        self._set_id()
+        self._set_settime()
+
+    def _set_id(self) -> None:
+        self.__id = self.__class__.__name__.lower() + '_' + _MF.new_code()
+
+    def get_id(self) -> str:
+        return self.__id
+
+    def _set_settime(self) -> str:
+        self.__settime = _MF.get_timestamp(unit=_MF.TIME_MILLISEC)
+
+    def get_settime(self) -> int:
+        """
+        To get the creation time in millisecond
+
+        Returns:
+        --------
+        return: int
+            The creation time in millisecond
+        """
+        return self.__settime
+
 
     @abstractmethod
-    def create_bot(self, bkr: str, stg: str, prcd: str, configs: dict) -> Bot:
+    def new_bot(self, capital_num: float, asset_str: str, strategy_str: str, broker_str: str, pair_str: str = None) -> str:
         """
-        To create a new Bot\n
-        :param bkr: name of a supported Broker
-        :param stg: name of a supported Strategy
-        :param prcd: code of the pair to Trade, i.e.: "BTC/USDT"
-        :param configs: holds additional configs for the Bot
-                        configs[{Bot}]      => Bot's configs
-                        configs[{Broker}]   => Broker's configs
-                        configs[{Strategy}] => Strategy's configs
-        :return: The Bot just created
-        """
-        pass
+        To create a new Bot
 
-    @abstractmethod
-    def get_log_id(self) -> str:
-        """
-        To get the id of the log session\n
-        :return: id of the log session
+        Parameters:
+        -----------
+        capital_num: float
+            The numeric value of the capital
+        asset_str: str
+            The Asset in witch the capital is expressed
+        strategy_str: str
+            The class name of the Strategy to use with Bot
+        broker_str: str
+            The class name of the Broker to use
+        pair_str: str = None
+            The only Pair to trade.
+            NOTE: if set to None then Bot will stalk all available Pair
+
+        Returns:
+        --------
+        retturns: str
+            The id of the new Bot created
         """
         pass
 
