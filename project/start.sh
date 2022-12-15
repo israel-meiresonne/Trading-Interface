b7cbb200b9502834e1f410bbe1afda04d2e3f312
israel_meiresonne97 <israelmeiresonne97@gmail.com>
2022-12-15 15:40:29 +0000

    strart.sh: generate draft file

--- project/start.sh
+++ project/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+PY_ENV=env_3.9.13/bin/activate
+FILE_DRAFT="start_draft.sh"
+draft=$(cat "$FILE_DRAFT" | sed "s#source .*#source $PY_ENV#") ; echo "$draft" > "$FILE_DRAFT"
+
 git branch | grep -E "^\* .+"
-source env_3.9.13/bin/activate
-python3 main.py  
+source "$PY_ENV"
+python3 main.py
