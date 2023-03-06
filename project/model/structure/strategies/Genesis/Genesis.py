6eee28caf0e26c840843a913e3847c59b98cda31
israel_meiresonne97 <israelmeiresonne97@gmail.com>
2023-03-06 15:57:47 +0200

    Genesis: buy and sell following market's trend on 5min

--- project/model/structure/strategies/Genesis/Genesis.py
+++ project/model/structure/strategies/Genesis/Genesis.py
@@ -1,10 +1,13 @@
 import time
 from typing import List
+
+import pandas as pd
+from config.Config import Config
+
 from model.structure.Broker import Broker
-
-
 from model.structure.database.ModelFeature import ModelFeature as _MF
 from model.structure.strategies.Strategy import Strategy
+from model.tools.FileManager import FileManager
 from model.tools.Map import Map
 from model.tools.MarketPrice import MarketPrice
 from model.tools.MyJson import MyJson
@@ -37,11 +40,13 @@ class Genesis(Strategy):
     # ••• TRADE UP
     # ——————————————————————————————————————————— SELF FUNCTION DOWN ——————————————————————————————————————————————————
     # ——————————————————————————————————————————— STATIC FUNCTION DOWN ————————————————————————————————————————————————
+    # ––––––––––––––––––––––––––––––––––––––––––– BACKTEST DOWN
 
     @classmethod
     def can_buy(cls, broker: Broker, pair: Pair, marketprices: Map) -> tuple[bool, dict]:
         vars_map = Map()
         period_1min = Broker.PERIOD_1MIN
+        period_5min = Broker.PERIOD_5MIN
         func_name = cls.can_buy.__name__
         _stack = cls.get_stack()
         """
@@ -51,13 +56,16 @@ class Genesis(Strategy):
         var_map_keys = _stack.get(func_name)
         index_now = -1
         if var_map_keys is None:
+            cls.is_tangent_market_trend_positive(vars_map, broker, pair, period_5min, marketprices, index_now)
             cls.is_supertrend_rising(vars_map, broker, pair, period_1min, marketprices, index_now)
             new_var_map_keys = vars_map.get_keys()
             for new_var_map_key in new_var_map_keys:
                 columns = list(vars_map.get(new_var_map_key).keys())
                 _stack.put(columns, func_name, new_var_map_key)
             var_map_keys = _stack.get(func_name)
-        can_buy = cls.is_supertrend_rising(vars_map, broker, pair, period_1min, marketprices, index_now)
+        # Check
+        can_buy = cls.is_tangent_market_trend_positive(vars_map, broker, pair, period_5min, marketprices, index_now) \
+            and cls.is_supertrend_rising(vars_map, broker, pair, period_1min, marketprices, index_now)
         # Report
         boolean_keys = var_map_keys[Map.condition]
         values_keys = var_map_keys[Map.value]
@@ -72,6 +80,7 @@ class Genesis(Strategy):
     def can_sell(cls, broker: Broker, pair: Pair, marketprices: Map) -> tuple[bool, dict]:
         vars_map = Map()
         period_1min = Broker.PERIOD_1MIN
+        period_5min = Broker.PERIOD_5MIN
         func_name = cls.can_sell.__name__
         _stack = cls.get_stack()
         """
@@ -81,13 +90,16 @@ class Genesis(Strategy):
         var_map_keys = _stack.get(func_name)
         index_now = -1
         if var_map_keys is None:
+            cls.is_tangent_market_trend_positive(vars_map, broker, pair, period_5min, marketprices, index_now)
             cls.is_supertrend_rising(vars_map, broker, pair, period_1min, marketprices, index_now)
             new_var_map_keys = vars_map.get_keys()
             for new_var_map_key in new_var_map_keys:
                 columns = list(vars_map.get(new_var_map_key).keys())
                 _stack.put(columns, func_name, new_var_map_key)
             var_map_keys = _stack.get(func_name)
-        can_sell = not cls.is_supertrend_rising(vars_map, broker, pair, period_1min, marketprices, index_now)
+        # Check
+        can_sell = not cls.is_tangent_market_trend_positive(vars_map, broker, pair, period_5min, marketprices, index_now) \
+            or not cls.is_supertrend_rising(vars_map, broker, pair, period_1min, marketprices, index_now)
         # Report
         boolean_keys = var_map_keys[Map.condition]
         values_keys = var_map_keys[Map.value]
@@ -117,6 +129,30 @@ class Genesis(Strategy):
         return is_rising
 
     @classmethod
+    def is_tangent_market_trend_positive(cls, vars_map: Map, broker: Broker, pair: Pair, period: int, marketprices: Map, index: int) -> bool:
+        period_str = broker.period_to_str(period)
+        marketprice = cls._marketprice(broker, pair, period, marketprices)
+        market_trend_df = cls.get_market_trend(period)
+        now_time = marketprice.get_time()
+        now_date = _MF.unix_to_date(now_time)
+        sub_market_trend_df = market_trend_df[market_trend_df.index <= now_time]
+        prev_index = index - 1
+        now_trend_date = sub_market_trend_df['market_date'].iloc[index]
+        prev_trend_date = sub_market_trend_df['market_date'].iloc[prev_index]
+        # Check
+        now_rise_rate = sub_market_trend_df['rise_rate'].iloc[index]
+        prev_rise_rate = sub_market_trend_df['rise_rate'].iloc[prev_index]
+        tangent_positive = now_rise_rate > prev_rise_rate
+        # Report
+        vars_map.put(tangent_positive,  Map.condition,  f'tangent_market_trend_positive_{period_str}[{index}]')
+        vars_map.put(now_date,          Map.value,      f'tangent_trend_now_date{period_str}[{index}]')
+        vars_map.put(now_trend_date,    Map.value,      f'tangent_trend_now_trend_date{period_str}[{index}]')
+        vars_map.put(prev_trend_date,   Map.value,      f'tangent_trend_prev_trend_date{period_str}[{prev_index}]')
+        vars_map.put(now_rise_rate,     Map.value,      f'tangent_trend_now_rise_rate{period_str}[{index}]')
+        vars_map.put(prev_rise_rate,    Map.value,      f'tangent_trend_prev_rise_rate{period_str}[{prev_index}]')
+        return tangent_positive
+
+    @classmethod
     def _backtest_loop_inner(cls, broker: Broker, marketprices: Map, pair: Pair, trade: dict, buy_conditions: list, sell_conditions: list) -> dict:
         period_1min = Broker.PERIOD_1MIN
         marketprice = cls._marketprice(broker, pair, period_1min, marketprices)
@@ -134,6 +170,27 @@ class Genesis(Strategy):
                 cls._backtest_trade_set_sell_order(broker, marketprices, trade, Order.TYPE_MARKET, exec_type=Map.close)
         return trade
 
+    # ––––––––––––––––––––––––––––––––––––––––––– BACKTEST UP
+    # ––––––––––––––––––––––––––––––––––––––––––– STATIC DOWN
+
+    @classmethod
+    def get_market_trend(cls, period: int) -> pd.DataFrame:
+        _MF.check_type(period, int)
+        MARKET_TRENDS = 'MARKET_TRENDS'
+        stage = Config.get(Config.STAGE_MODE)
+        if stage == Config.STAGE_1:
+            _stack = cls.get_stack()
+            market_trend_df = _stack.get(MARKET_TRENDS, period)
+            if market_trend_df is None:
+                strategy_storage_dir = Config.get(Config.DIR_STRATEGY_STORAGE)
+                market_trend_file = f"{strategy_storage_dir}{cls.__name__}/market_trend/supertrend/{period}.csv"
+                project_dir = FileManager.get_project_directory()
+                market_trend_df = pd.read_csv(project_dir + market_trend_file, index_col=0)
+                _stack.put(market_trend_df, MARKET_TRENDS, period)
+        else:
+            raise Exception(f"Behavior not implemeted for this stage '{stage}'")
+        return market_trend_df
+
     @staticmethod
     def json_instantiate(object_dic: dict) -> object:
         _class_token = MyJson.get_class_name_token()
@@ -141,5 +198,6 @@ class Genesis(Strategy):
         exec(MyJson.get_executable())
         return instance
 
+    # ––––––––––––––––––––––––––––––––––––––––––– STATIC UP
     # ——————————————————————————————————————————— STATIC FUNCTION UP ——————————————————————————————————————————————————
     
