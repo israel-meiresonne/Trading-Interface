61141ee755f6129e5e809a44c272a8893da8f96b
israel_meiresonne97 <israelmeiresonne97@gmail.com>
2023-03-28 15:33:06 +0300

    Genesis: change buy condition of Keltner ROI from 1% to 2%

--- project/model/structure/strategies/Genesis/Genesis.py
+++ project/model/structure/strategies/Genesis/Genesis.py
@@ -89,7 +89,7 @@ class Genesis(Strategy):
 
     @classmethod
     def can_buy(cls, broker: Broker, pair: Pair, marketprices: Map) -> tuple[bool, dict]:
-        TRIGGE_KELTNER = 1/100
+        TRIGGE_KELTNER = 2/100
         vars_map = Map()
         period_1min = Broker.PERIOD_1MIN
         period_5min = Broker.PERIOD_5MIN
