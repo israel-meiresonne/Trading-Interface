8ba330886cc7229b14113de713ea15d9ce39e8d8
israel_meiresonne97 <israelmeiresonne97@gmail.com>
2023-03-28 18:46:04 +0300

    Genesis: buy if tangent of MACD(1min)[Histogram] is positive

--- project/model/structure/strategies/Genesis/Genesis.py
+++ project/model/structure/strategies/Genesis/Genesis.py
@@ -113,13 +113,15 @@ class Genesis(Strategy):
         func_and_params = [
             {Map.callback: cls.is_tangent_market_trend_positive,    Map.param: dict(vars_map=vars_map, broker=broker, pair=pair, period=period_5min, marketprices=marketprices, index=now_index)},
             {Map.callback: cls.is_keltner_roi_above_trigger,        Map.param: dict(vars_map=vars_map, broker=broker, pair=pair, period=period_1min, marketprices=marketprices, trigge_keltner=TRIGGE_KELTNER, index=now_index)},
+            {Map.callback: cls.is_tangent_macd_line_positive,       Map.param: dict(vars_map=vars_map, broker=broker, pair=pair, period=period_1min, marketprices=marketprices, index=now_index, line_name=Map.histogram)},
             {Map.callback: cls.is_supertrend_rising,                Map.param: dict(vars_map=vars_map, broker=broker, pair=pair, period=period_1min, marketprices=marketprices, index=now_index)}
         ]
         header_dict = cls._can_buy_sell_set_headers(this_func, func_and_params)
         # Check
         can_buy = cls.is_tangent_market_trend_positive(**func_and_params[0][Map.param]) \
             and cls.is_keltner_roi_above_trigger(**func_and_params[1][Map.param]) \
-            and cls.is_supertrend_rising(**func_and_params[2][Map.param])
+            and cls.is_tangent_macd_line_positive(**func_and_params[2][Map.param]) \
+            and cls.is_supertrend_rising(**func_and_params[3][Map.param])
         # Report
         report = cls._can_buy_sell_new_report(this_func, header_dict, can_buy, vars_map)
         buy_limit = vars_map.get(Map.value, k_keltner_middle_1min)
@@ -228,6 +230,24 @@ class Genesis(Strategy):
         vars_map.put(now_rise_rate,     Map.value,      f'tangent_trend_now_rise_rate{period_str}[{index}]')
         vars_map.put(prev_rise_rate,    Map.value,      f'tangent_trend_prev_rise_rate{period_str}[{prev_index}]')
         return tangent_positive
+
+    @classmethod
+    def is_tangent_macd_line_positive(cls, vars_map: Map, broker: Broker, pair: Pair, period: int, marketprices: Map, index: int, line_name: str, macd_params: dict = {}) -> bool:
+        period_str = broker.period_to_str(period)
+        marketprice = cls._marketprice(broker, pair, period, marketprices)
+        macd_lines = marketprice.get_macd(**macd_params).get_map()
+        macd_line = list(macd_lines[line_name])
+        macd_line.reverse()
+        # Check
+        prev_index = index - 1
+        tangent_macd_line_positive = macd_line[index] > macd_line[prev_index]
+        # Put
+        param_str = _MF.param_to_str(macd_params)
+        vars_map.put(tangent_macd_line_positive,    Map.condition,  f'is_tangent_macd_line_positive_{param_str}_{period_str}[{index}]')
+        vars_map.put(line_name,                     Map.value,      f'is_tangent_macd_line_positive_line_name_{param_str}_{period_str}[{index}]')
+        vars_map.put(macd_line[index],              Map.value,      f'is_tangent_macd_line_positive_macd_line_{param_str}_{period_str}[{index}]')
+        vars_map.put(macd_line[prev_index],         Map.value,      f'is_tangent_macd_line_positive_macd_line_{param_str}_{period_str}[{prev_index}]')
+        return tangent_macd_line_positive
 
     @classmethod
     def _backtest_loop_inner(cls, broker: Broker, marketprices: Map, pair: Pair, trades: list[dict], trade: dict, buy_conditions: list, sell_conditions: list) -> None:
